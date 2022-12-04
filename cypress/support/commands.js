0dcca4a270de814ac55a49f61aa8bc35a80d8798
victor <victor.fernandes@radixeng.com.br>
2022-12-04 16:33:36 -0300

    adding new cypress commands

---
+++ cypress/support/commands.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+import assertive from "../Shared/assertive";
+import "cypress-file-upload";
+
+const { ASSERTIVE } = assertive.assertive();
+
+Cypress.Commands.add("compareText", (locator, text) => {
+  cy.get(locator).should(ASSERTIVE.COMPARE_TEXT, text);
+});
+
+Cypress.Commands.add("isVisible", (locator) => {
+  cy.get(locator).should(ASSERTIVE.VISIBLE);
+});
+
+Cypress.Commands.add("isClickable", (locator) => {
+  cy.get(locator).should(ASSERTIVE.VISIBLE);
+  cy.get(locator).click();
+});
+
+Cypress.Commands.add("verifyUrl", (text) => {
+  cy.url().should(ASSERTIVE.INCLUDE_TEXT, text);
+});
+
+Cypress.Commands.add("insertText", (locator, text) => {
+  cy.get(locator).should(ASSERTIVE.VISIBLE);
+  cy.get(locator).type(text);
+});
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
+});
